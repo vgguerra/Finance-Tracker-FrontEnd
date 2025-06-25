d78738b82f6078aa9e60cde3585f8b83cb50c0df
vgguerra <victorevandro94@gmail.com>
2025-06-25 02:28:41 -0300

    Sytled Component

--- src/pages/TransactionsPage.jsx
+++ src/pages/TransactionsPage.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { createTransaction, getAllTransactions } from "../services/TransactionsService";
+import {
+  createTransaction,
+  getAllTransactions,
+} from "../services/TransactionsService";
 
 const TransactionsPage = () => {
   const [transactions, setTransactions] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-    const [newTransactionDescription, setNewTransactionDescription] = useState('');
-    const [newTransactionValue, setNewTransactionValue] = useState('');
+  const [newTransactionDescription, setNewTransactionDescription] =
+    useState("");
+  const [newTransactionValue, setNewTransactionValue] = useState("");
 
   const fetchTransactions = async () => {
     try {
@@ -16,7 +20,7 @@ const TransactionsPage = () => {
       setTransactions(response.data);
       setError(null);
     } catch (err) {
-      setError("Não foi possível carregar as transações."); 
+      setError("Não foi possível carregar as transações.");
       console.error(err);
     } finally {
       setLoading(false);
@@ -27,26 +31,29 @@ const TransactionsPage = () => {
     fetchTransactions();
   }, []);
 
-    const handleCreateTransaction = async (e) => {
-      e.preventDefault();
-      if (!newTransactionDescription.trim() || !newTransactionValue) {
-          alert('Todos os campos devem estar preenchidos.');
-          return;
-      }
-      try {
-        await createTransaction({ description: newTransactionDescription,value: newTransactionValue });
-        alert('Transação criada com sucesso!');
-        setNewTransactionDescription(''); 
-        setNewTransactionValue('');
-        fetchTransactions();    
-      } catch (err) {
-        setError('Erro ao criar a Transação.');
-        console.error(err);
-      }
-    };
+  const handleCreateTransaction = async (e) => {
+    e.preventDefault();
+    if (!newTransactionDescription.trim() || !newTransactionValue) {
+      alert("Todos os campos devem estar preenchidos.");
+      return;
+    }
+    try {
+      await createTransaction({
+        description: newTransactionDescription,
+        value: newTransactionValue,
+      });
+      alert("Transação criada com sucesso!");
+      setNewTransactionDescription("");
+      setNewTransactionValue("");
+      fetchTransactions();
+    } catch (err) {
+      setError("Erro ao criar a Transação.");
+      console.error(err);
+    }
+  };
 
   if (loading) {
-    return <p>Carregando transações...</p>; 
+    return <p>Carregando transações...</p>;
   }
 
   if (error) {
@@ -67,12 +74,32 @@ const TransactionsPage = () => {
       <h1>Minhas Transações</h1>
       <ul>
         {transactions.map((transaction) => (
-          <li key={transaction.id} style={{ marginBottom: '10px', border: '1px solid #ccc', padding: '8px', borderRadius: '4px' }}>
-            <p><strong>Descrição:</strong> {transaction.description}</p>
-            <p><strong>Valor:</strong> R$ {transaction.value.toFixed(2)}</p>
-            <p><strong>Data:</strong> {transaction.date}</p>
-            <p><strong>Tipo:</strong> {transaction.type === 'EXPENSE' ? 'Despesa' : 'Receita'}</p>
-            <p><strong>Categoria:</strong> {transaction.category ? transaction.category.name : 'N/A'}</p>
+          <li
+            key={transaction.id}
+            style={{
+              marginBottom: "10px",
+              border: "1px solid #ccc",
+              padding: "8px",
+              borderRadius: "4px",
+            }}
+          >
+            <p>
+              <strong>Descrição:</strong> {transaction.description}
+            </p>
+            <p>
+              <strong>Valor:</strong> R$ {transaction.value.toFixed(2)}
+            </p>
+            <p>
+              <strong>Data:</strong> {transaction.date}
+            </p>
+            <p>
+              <strong>Tipo:</strong>{" "}
+              {transaction.type === "EXPENSE" ? "Despesa" : "Receita"}
+            </p>
+            <p>
+              <strong>Categoria:</strong>{" "}
+              {transaction.category ? transaction.category.name : "N/A"}
+            </p>
           </li>
         ))}
       </ul>
